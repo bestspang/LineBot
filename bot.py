581467fc025a8ef9f1c0263d17f82a9726b44465
bestspang <bestspang@hotmail.com>
2018-07-04 22:27:15 +0700

    update!

--- bot.py
+++ bot.py
@@ -1,24 +1,19 @@
 from flask import Flask, request, abort
-import json
-import requests
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+import json, requests
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('Z7FgW5zgSO1G9BaHiMJOCKTByoH6Fl9gFIam59JdkfVXaavM8k8DEsEfLZpWmBlNDbWv/q4wYA0mY/gJWLfNUBFX8yNp+5A5THgSjLzx6DTLVi5x69Ejbd1JRLBOtiS7/HoOmKHJDvmmlDEt2DXj1QdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('1b8e881368efe90738ce5c3341898c35')
 
+@app.route("/")
+def hello():
+    return "This is BP_LINEBOT!"
 
-@app.route("/callback", methods=['POST'])
+@app.route("/webhook", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
@@ -33,20 +28,11 @@ def callback():
     except InvalidSignatureError:
         abort(400)
 
-    return 'OK'
-
-@app.route("/webhook", methods=['GET', 'POST'])
-def webhook():
-    if request.method == 'POST':
-        return 'OK'
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
-
-
 
 if __name__ == "__main__":
     app.run()
