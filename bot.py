52dacb5369a68ef27d26999541680aeb4e8f1f1d
Kongphop <bestspang@gmail.com>
2019-05-26 19:18:23 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,7 +60,6 @@ def getQuote():
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',})
     site_request = requests.get(url, headers=headers)
-    site_content = soup(site_request.content, "html.parser")
     data = json.loads(site_request.text)
 
     quote = data['contents']['quotes']
