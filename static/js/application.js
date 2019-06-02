bb10692cefa07d38da37d7ab12d7678530b67c45
Kongphop <bestspang@gmail.com>
2019-06-03 01:41:03 +0700

    Update application.js

--- static/js/application.js
+++ static/js/application.js
@@ -1,7 +1,6 @@
-
 $(document).ready(function(){
     //connect to the socket server.
-    var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+    var socket = io.connect('https://' + document.domain + ':' + location.port + '/test');
     var numbers_received = [];
 
     //receive details from server
