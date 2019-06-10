43356963d3f87587489bb9b9977487f444fd8624
Kongphop <bestspang@gmail.com>
2019-06-11 00:01:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -657,14 +657,14 @@ def handle_message(event):
             TextSendMessage(text=response_text))
         return 0
 
-    if '!test' in text:
+    if text = '!test':
         is_approve_new_member()
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=text))
         return 0
 
-    if '!help' in text:
+    if text == '!help':
         is_approve_new_member()
         line_bot_aปpi.reply_message(
             event.reply_token,
@@ -672,7 +672,7 @@ def handle_message(event):
         return 0
 
     if 'ใคร' in words_list and ('ทำงานอยู่' in text or 'อยู่ที่ทำงาน' in text):
-        text = who_work()
+        text == who_work()
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=text))
