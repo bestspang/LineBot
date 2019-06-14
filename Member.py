81a7a4e8082b0e2dea9789f59056e186e65218a7
Kongphop <bestspang@gmail.com>
2019-06-14 17:21:49 +0700

    Update Member.py

--- Member.py
+++ Member.py
@@ -1,32 +1,31 @@
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread, pprint
 
-
 class Member:
     def __init__(self):
         #self.name = name
         #self.id = id
         #self.line_id = line_id
         #self.marks = []
-        scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-        creds = ServiceAccountCredentials.from_json_keyfile_name('BPLINEBOT-57c70064e9b9.json', scope)
-        client = gspread.authorize(creds)
+        self.scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+        self.creds = ServiceAccountCredentials.from_json_keyfile_name('BPLINEBOT-57c70064e9b9.json', scope)
+        self.client = gspread.authorize(creds)
 
     def member_rank(input):
         if is_member(input) and is_approve(input):
-            sheet = client.open('lineUser').worksheet('user')
+            sheet = self.client.open('lineUser').worksheet('user')
             user_id = sheet.col_values(3)[1:]
             return sheet.col_values(6)[1:][user_id.index(input)]
         else:
             return False
 
     def get_all_member_ids():
-            sheet = client.open('lineUser').worksheet('user')
+            sheet = self.client.open('lineUser').worksheet('user')
             user_id = sheet.col_values(3)[1:]
             return user_id
 
     def is_member(input):
-            sheet = client.open('lineUser').worksheet('user')
+            sheet = self.client.open('lineUser').worksheet('user')
             user_id = sheet.col_values(3)[1:]
             if input in user_id:
                 return True
@@ -34,7 +33,7 @@ class Member:
                 return False
 
     def is_approve(input):
-            sheet = client.open('lineUser').worksheet('user')
+            sheet = self.client.open('lineUser').worksheet('user')
             user_id = sheet.col_values(3)[1:]
             if input in user_id and sheet.col_values(4)[1:][user_id.index(input)] == "APPROVE":
                 return True
@@ -42,7 +41,7 @@ class Member:
                 return False
 
     def add_member(input):
-            sheet = client.open('lineUser').worksheet('user')
+            sheet = self.client.open('lineUser').worksheet('user')
             profile = line_bot_api.get_profile(input)
             now = datetime.datetime.now()
             row_num = len(sheet.col_values(3)[1:])
@@ -53,7 +52,7 @@ class Member:
             line_bot_api.push_message(to, TextSendMessage(text=profile.display_name + 'ได้สมัครสมาชิก!'))
 
     def is_approve_new_member():
-        sheet = client.open('lineUser').worksheet('user')
+        sheet = self.client.open('lineUser').worksheet('user')
         name = sheet.col_values(2)[-1]
         confirm_template = ConfirmTemplate(text='Approve หรือ ไม่?', actions=[
             PostbackAction(label='Yes',text='Yes!',data='member_yes'),
@@ -68,7 +67,7 @@ class Member:
 
     def approve_member(boo):
         if boo == 1:
-            sheet = client.open('lineUser').worksheet('user')
+            sheet = self.client.open('lineUser').worksheet('user')
             row_num = len(sheet.col_values(3)[1:])
             sheet.update_cell(row_num+1, 4, "APPROVE")
             sheet.update_cell(row_num+1, 6, "1")
