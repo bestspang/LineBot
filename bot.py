38e1f624341f74082fe9229a4a2b8d8d0810055e
Kongphop <bestspang@gmail.com>
2019-06-17 23:18:41 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -215,12 +215,12 @@ def init_scheduler():
     #scheduler.add_job(func=print_date_time, trigger="interval", seconds=3)
     job = scheduler.add_job(print_date_time,"cron",
                 day_of_week='mon-fri',
-                hour=22, minute=46)# args=[text]
+                hour=23, minute=25)# args=[text]
     scheduler.start()
     # Shut down the scheduler when exiting the app
     # atexit.register(lambda: scheduler.shutdown())
 
-if __name__ != '__main__' or not app.debug or os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
+if not app.debug or os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
     print("scheduler True!")
     init_scheduler()
 else:
