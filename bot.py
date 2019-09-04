5216d7837207946223b35ff56f0abff27803bded
Kongphop <bestspang@gmail.com>
2019-09-04 17:39:23 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -258,7 +258,10 @@ def make_static_tmp_dir():
 def detect_intent_texts(project_id, session_id, text, language_code):
     import dialogflow_v2 as dialogflow
     key_file_path = "./BPLINEBOT-0106b42afbf3.json"
-    credentials = Credentials.from_service_account_file(key_file_path)
+    # credentials = ServiceAccountCredentials.from_json_keyfile_name('bplinebot-3ccea59ad6d6.json', scope)
+    credentials = ServiceAccountCredentials.from_service_account_file(key_file_path)
+    # credentials = service_account.Credentials.from_service_account_file(key_file_path)
+    # credentials = Credentials.from_service_account_file(key_file_path)
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(project_id, session_id)
 
